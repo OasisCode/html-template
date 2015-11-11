f97de393aaea1ee56433eebfb8f9f446cdc1a828
Shayan Nahrvar <shayan@oasiscode.com>
2015-11-11 13:20:40 -0500

    Initial Build
    Basic Boilerplate including: SASS, Bootstrap, JQuery, FontAwesome,
    SmoothScroll, and Slick Carousel

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang="en">
+	<head>
+	  	<meta charset="utf-8">
+
+		<title>Entefy Careers</title>
+		<meta name="description" content="Entefy Career Opportunities Page">
+		<link rel="stylesheet" href="css/style.css?v=1.0">
+		<link rel="stylesheet" href="includes/slick/slick.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	</head>
+
+	<body>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script src="includes/slick/slick.min.js"></script>
+		<script src="js/SmoothScroll.js"></script>
+		<script src="js/custom.js"></script>
+	</body>
+</html>
